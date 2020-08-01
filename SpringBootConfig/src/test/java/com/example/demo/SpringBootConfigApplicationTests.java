06ab1a973137ed999825c3f27f7218b38d1ef140
ygf2018 <39176651+ygf2018@users.noreply.github.com>
2020-08-01 11:49:37 +0800

    CONFIG

---
+++ SpringBootConfig/src/test/java/com/example/demo/SpringBootConfigApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootTest
+public class SpringBootConfigApplicationTests {
+
+	@Test
+	public
+	 void contextLoads() {
+	}
+
+}
